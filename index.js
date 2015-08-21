8f96be8cbe1cc07ac5f472f8cf7fc4e842218f2a
Andris Reinman <andris@kreata.ee>
2015-08-21 14:29:43 +0300

    Initial version v1.0.0

---
+++ index.js
@@ -0,0 +1,117 @@
+'use strict';
+
+var jsrsasign = require('jsrsasign');
+var fs = require('fs');
+var path = require('path');
+var Handlebars = require('handlebars');
+var uuid = require('uuid');
+
+var templates = {
+    imap: Handlebars.compile(fs.readFileSync(path.join(__dirname, 'templates', 'imap.plist'), 'utf-8'))
+};
+
+module.exports = {
+    sign: function(value, options, callback) {
+        options = options || {};
+
+        var pem;
+        var der;
+        var params = {
+
+            content: {
+                // the signed content needs to be a normal unicode string
+                str: (value || '').toString('utf-8')
+            },
+
+            // join ca certs with signer cert into single array and ensure the values are strings, not Buffer object
+            certs: [].concat(options.ca || []).concat(options.cert || []).map(function(cert) {
+                return (cert || '').toString();
+            }),
+
+            signerInfos: [{
+
+                // sha256, sha512, sha384, sha224, sha1, md5, ripemd160
+                hashAlg: options.hashAlg || 'sha256',
+
+                // If signingTime is true, add SigingTime signed attribute
+                sAttr: options.signingTime ? {
+                    SigningTime: {}
+                } : {},
+
+                signerCert: (options.cert || '').toString(),
+                signerPrvKey: (options.key || '').toString(),
+
+                // SHA256withRSA, SHA512withRSA, SHA384withRSA, SHA224withRSA, SHA1withRSA,MD5withRSA
+                // RIPEMD160withRSA, SHA256withECDSA, SHA512withECDSA, SHA384withECDSA, SHA224withECDSA, SHA1withECDSA
+                // SHA256withSA, SHA512withSA, SHA384withSA, SHA224withSA, SHA1withDSA
+                sigAlg: options.sigAlg || 'SHA256withRSA'
+            }]
+        };
+
+        try {
+            pem = jsrsasign.asn1.cms.CMSUtil.newSignedData(params).getPEM();
+            der = new Buffer(jsrsasign.KEYUTIL.getHexFromPEM(pem, 'CMS'), 'hex');
+        } catch (E) {
+            return setImmediate(function() {
+                callback(E);
+            });
+        }
+
+        return setImmediate(function() {
+            callback(null, der);
+        });
+    },
+
+    getEmailConfig: function(options) {
+        var imap = options.imap || {};
+        var smtp = options.smtp || {};
+
+        var data = {
+            emailAddress: options.emailAddress || 'admin@localhost',
+
+            organization: options.organization || false,
+            identifier: options.identifier || 'com.kreata.anonymous',
+
+            displayName: options.displayName || 'Mail Account',
+            displayDescription: options.displayDescription,
+
+            accountName: options.accountName || 'IMAP Account',
+            accountDescription: options.accountDescription || false,
+
+            imap: {
+                hostname: imap.hostname || 'localhost',
+                port: imap.port || (imap.secure ? 993 : 143),
+                secure: imap.hasOwnProperty('secure') ? !!imap.secure : imap.port === 993,
+                username: imap.username || options.emailAddress || 'anonymous',
+                password: imap.password || ''
+            },
+
+            smtp: {
+                hostname: smtp.hostname || 'localhost',
+                port: smtp.port || (smtp.secure ? 465 : 587),
+                secure: smtp.hasOwnProperty('secure') ? !!smtp.secure : smtp.port === 465,
+                username: smtp.username || false,
+                password: smtp.password || false
+            },
+
+            contentUuid: options.contentUuid || uuid.v4(),
+            plistUuid: options.plistUuid || uuid.v4()
+        };
+
+        return templates.imap(data);
+    },
+
+    getSignedEmailConfig: function(options, callback) {
+        options = options || {};
+
+        var plist;
+
+        try {
+            plist = module.exports.getEmailConfig(options);
+        } catch (E) {
+            return callback(E);
+        }
+
+        return module.exports.sign(plist, options.keys, callback);
+    }
+};
---
+++ test/mobileconfig-unit.js
@@ -0,0 +1,182 @@
+'use strict';
+
+var mobileconfig = require('../index');
+var chai = require('chai');
+var expect = chai.expect;
+var fs = require('fs');
+var plist = require('plist');
+
+chai.Assertion.includeStack = true;
+
+describe('mobileconfig unit tests', function() {
+
+    describe('#sign', function() {
+
+        it('should sign value', function(done) {
+
+            var value = 'Hello ŠŽ 👻';
+            var options = {
+                key: fs.readFileSync(__dirname + '/fixtures/key.pem'),
+                cert: fs.readFileSync(__dirname + '/fixtures/cert.pem'),
+                hashAlg: 'sha256',
+                sigAlg: 'SHA256withRSA',
+                signingTime: false
+            };
+
+            mobileconfig.sign(value, options, function(err, data) {
+                expect(err).to.not.exist;
+                expect(data.toString('binary')).to.equal(fs.readFileSync(__dirname + '/fixtures/signed-string.der', 'binary'));
+                done();
+            });
+
+        });
+
+        it('should return an error', function(done) {
+
+            var value = 'Hello ŠŽ 👻';
+            var options = {
+                key: fs.readFileSync(__dirname + '/fixtures/key.pem'),
+                cert: fs.readFileSync(__dirname + '/fixtures/key.pem'),
+                hashAlg: 'sha256',
+                sigAlg: 'SHA256withRSA',
+                signingTime: false
+            };
+
+            mobileconfig.sign(value, options, function(err, data) {
+                expect(err).to.exist;
+                expect(data).to.not.exist;
+                done();
+            });
+
+        });
+
+    });
+
+    describe('#getEmailConfig', function() {
+        it('should generate valid plist', function() {
+            var options = {
+                emailAddress: 'my-email-address@gmail.com',
+
+                organization: 'My Company',
+                identifier: 'com.my.company',
+
+                displayName: 'My Gmail Account',
+                displayDescription: 'Install this profile to auto configure your Gmail account',
+
+                accountName: 'IMAP Config',
+                accountDescription: 'Configure your Gmail account',
+
+                imap: {
+                    hostname: 'imap.gmail.com',
+                    port: 993,
+                    secure: true,
+                    username: 'my-email-address@gmail.com',
+                    password: 'mypass'
+                },
+
+                smtp: {
+                    hostname: 'smtp.gmail.com',
+                    port: 587,
+                    secure: false,
+                    username: 'my-email-address@gmail.com',
+                    password: false // use the same password as for IMAP
+                },
+
+                contentUuid: 'abcdef',
+                plistUuid: 'ghijklmn'
+            };
+
+            var emailConfig = plist.parse(mobileconfig.getEmailConfig(options));
+
+            expect(emailConfig).to.deep.equal({
+                PayloadContent: [{
+                    EmailAccountDescription: 'Configure your Gmail account',
+                    EmailAccountType: 'EmailTypeIMAP',
+                    EmailAddress: 'my-email-address@gmail.com',
+                    IncomingMailServerAuthentication: 'EmailAuthPassword',
+                    IncomingMailServerHostName: 'imap.gmail.com',
+                    IncomingMailServerPortNumber: 993,
+                    IncomingMailServerUseSSL: true,
+                    IncomingMailServerUsername: 'my-email-address@gmail.com',
+                    IncomingPassword: 'mypass',
+                    OutgoingMailServerAuthentication: 'EmailAuthPassword',
+                    OutgoingMailServerHostName: 'smtp.gmail.com',
+                    OutgoingMailServerPortNumber: 587,
+                    OutgoingMailServerUseSSL: false,
+                    OutgoingMailServerUsername: 'my-email-address@gmail.com',
+                    OutgoingPasswordSameAsIncomingPassword: true,
+                    PayloadDescription: 'Configures email account.',
+                    PayloadDisplayName: 'IMAP Config',
+                    PayloadIdentifier: 'com.my.company',
+                    PayloadOrganization: 'My Company',
+                    PayloadType: 'com.apple.mail.managed',
+                    PayloadUUID: 'abcdef',
+                    PayloadVersion: 1,
+                    PreventAppSheet: false,
+                    PreventMove: false,
+                    SMIMEEnabled: false
+                }],
+                PayloadDescription: 'Install this profile to auto configure your Gmail account',
+                PayloadDisplayName: 'My Gmail Account',
+                PayloadIdentifier: 'com.my.company',
+                PayloadOrganization: 'My Company',
+                PayloadRemovalDisallowed: false,
+                PayloadType: 'Configuration',
+                PayloadUUID: 'ghijklmn',
+                PayloadVersion: 1
+            });
+        });
+    });
+
+    describe('#getSignedEmailConfig', function() {
+        it('should not return an error', function(done) {
+            var options = {
+                emailAddress: 'my-email-address@gmail.com',
+
+                organization: 'My Company',
+                identifier: 'com.my.company',
+
+                displayName: 'My Gmail Account',
+                displayDescription: 'Install this profile to auto configure your Gmail account',
+
+                accountName: 'IMAP Config',
+                accountDescription: 'Configure your Gmail account',
+
+                imap: {
+                    hostname: 'imap.gmail.com',
+                    port: 993,
+                    secure: true,
+                    username: 'my-email-address@gmail.com',
+                    password: 'mypass'
+                },
+
+                smtp: {
+                    hostname: 'smtp.gmail.com',
+                    port: 587,
+                    secure: false,
+                    username: 'my-email-address@gmail.com',
+                    password: false // use the same password as for IMAP
+                },
+
+                contentUuid: 'abcdef',
+                plistUuid: 'ghijklmn',
+
+                keys: {
+                    key: fs.readFileSync(__dirname + '/fixtures/key.pem'),
+                    cert: fs.readFileSync(__dirname + '/fixtures/cert.pem'),
+                    hashAlg: 'sha256',
+                    sigAlg: 'SHA256withRSA',
+                    signingTime: false
+                }
+            };
+
+            mobileconfig.getSignedEmailConfig(options, function(err, data) {
+                expect(err).to.not.exist;
+                expect(data).to.exist;
+
+                done();
+            });
+        });
+    });
+
+});
